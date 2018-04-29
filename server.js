36582ceeee6db4ebc159e8632d5dbc229d9c7bc1
Joao Pereira <joaopereira.19@gmail.com>
2018-04-29 17:30:19 +0100

    update server

--- server.js
+++ server.js
@@ -1,3 +1,52 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var id = 1;
+var sessions = {};
+
+app.post('/sessionCreator', function (req, res) {
+   sessions[""+id] = {"domain": req["domain"], "client_key": req["client_key"] }; // lacks proof of id
+   data = {};
+   data["session_id"] = ""+id;
+   id++;
+   console.log(data);
+   res.end(JSON.stringify(data));
+});
+
+app.get('/session/:id', function (req, res) {
+   var rcv_id = req.params.id;
+   var authentication = req["identification"];
+
+   var session = sessions[rcv_id];
+   // lacks identity verification
+   if(session !== undefined && session.hidden_user !== undefined) {
+      data = {};
+      data["hidden_user"] = session["hidden_user"];
+      data["hidden_password"] = session["hidden_password"];
+      console.log("Session " + rcv_id + " concluded");
+      res.end(JSON.stringigy(data));
+   } else {
+      res.end();
+   }
+});
+
+app.post('/session/:id', function (req, res) {
+   session = sessions[req.params.id];
+   if(session == undefined) {
+       res.end();
+   }
+   session["hidden_user"] = req["hidden_user"];
+   session["hidden_password"] = req["hidden_password"];
+   res.end();
+});
+
+   
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
